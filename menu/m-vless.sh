beee71fe0caba62d03540fc2a707371c1254d850
kidarr <rizki.arifananda@gmail.com>
2023-04-02 11:06:58 +0700

    Kidar Script

---
+++ menu/m-vless.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+CEKEXPIRED () {
+    today=$(date -d +1day +%Y-%m-%d)
+    Exp1=$(curl -sS https://raw.githubusercontent.com/Kidarr/1/main/permission/ip | grep $MYIP | awk '{print $3}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
+    else
+    echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
+    
+    exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/Kidarr/1/main/permission/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+CEKEXPIRED
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+
+exit 0
+fi
+clear
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;100;33m       • VLESS AKUN •         \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e " [\e[36m•1\e[0m] Create AKUN Vless "
+echo -e " [\e[36m•2\e[0m] Trial AKUN Vless "
+echo -e " [\e[36m•3\e[0m] Extending AKUN Vless "
+echo -e " [\e[36m•4\e[0m] Delete AKUN Vless "
+echo -e " [\e[36m•5\e[0m] Check User Login Vless "
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mKEMBALI\033[0m"
+echo -e ""
+echo -e   "TEKAN x ATAU [ Ctrl+C ] • UNTUK KEMBALI"
+echo ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " PILIH MENU :  "  opt
+echo -e ""
+case $opt in
+1) clear ; add-vless ; exit ;;
+2) clear ; trialvless ; exit ;;
+3) clear ; renew-vless ; exit ;;
+4) clear ; del-vless ; exit ;;
+5) clear ; cek-vless ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo "salah tekan " ; sleep 1 ; m-sshovpn ;;
+esac
